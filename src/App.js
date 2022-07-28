a7db01416be6c4bb66643541503553cbfeb2a5cf
Imam Robani <imam.robani@sicepat.com>
2022-07-29 06:31:51 +0700

    feat: structure folder project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <Text>Hello World</Text>
     </View>
   );
 };
