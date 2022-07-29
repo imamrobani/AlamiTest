89093a8384145ea2124e6b2d9462307bf38ff982
Imam Robani <imam.robani@sicepat.com>
2022-07-29 08:43:21 +0700

    chore: custom bottom tab navigator

---
+++ src/assets/Icons/index.js
@@ -0,0 +1,6 @@
+import IcHomeOff from './ic-home-off.svg';
+import IcHomeOn from './ic-home-on.svg';
+import IcProfileOff from './ic-profile-off.svg';
+import IcProfileOn from './ic-profile-on.svg';
+
+export {IcHomeOff, IcHomeOn, IcProfileOff, IcProfileOn};
