91802e8f2530e701f804c4ff10591fad9762a759
Imam Robani <imam.robani@sicepat.com>
2022-07-31 22:04:53 +0700

    fix: image size

--- src/assets/Dummy/index.js
+++ src/assets/Dummy/index.js
@@ -1,6 +1,5 @@
 import Photo1 from './photo1.png';
 import Photo2 from './photo2.png';
-import Mario1 from './mario1.png';
 import Mario2 from './mario2.png';
 
-export {Photo1, Photo2, Mario1, Mario2};
+export {Photo1, Photo2, Mario2};
